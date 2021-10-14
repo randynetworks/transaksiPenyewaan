325c5d9f7e24322a5c55e1cfc008c6182cdf7d40
randynetworks <randyramadhan1973@gmail.com>
2021-10-14 12:51:44 +0700

    feat: membuat tampilan

--- transaksiPenyewaan/Form1.Designer.vb
+++ transaksiPenyewaan/Form1.Designer.vb
@@ -22,10 +22,370 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.tb_kode = New System.Windows.Forms.TextBox()
+        Me.tb_nama = New System.Windows.Forms.TextBox()
+        Me.tb_alamat = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.cb_jenis = New System.Windows.Forms.ComboBox()
+        Me.tb_harga_sewa = New System.Windows.Forms.TextBox()
+        Me.tb_jml = New System.Windows.Forms.TextBox()
+        Me.tb_potongan = New System.Windows.Forms.TextBox()
+        Me.tb_sub_harga = New System.Windows.Forms.TextBox()
+        Me.tb_grand = New System.Windows.Forms.TextBox()
+        Me.tb_pajak = New System.Windows.Forms.TextBox()
+        Me.tb_sub_grand = New System.Windows.Forms.TextBox()
+        Me.btn_keluar = New System.Windows.Forms.Button()
+        Me.btn_new = New System.Windows.Forms.Button()
+        Me.dtp_tanggal = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(268, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CV. AGUNG MUDA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(316, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "MENYEWAKAN ALAT PESTA (PERNIKAHAN, SUNATAN, DLL)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(352, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jl. Pelajar Pejuang No,. 45 Bandung Telp. (022) 6754390, (022) 6754287"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(443, 31)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "NPM: 18402137"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(443, 49)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(147, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "NAMA: RANDY RAMADHAN"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 114)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "KODE TRANSAKSI"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 142)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "TANGGAL"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(22, 200)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "ALAMAT"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(22, 172)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(107, 13)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "NAMA PELANGGAN"
+        '
+        'tb_kode
+        '
+        Me.tb_kode.Location = New System.Drawing.Point(158, 111)
+        Me.tb_kode.Name = "tb_kode"
+        Me.tb_kode.Size = New System.Drawing.Size(100, 20)
+        Me.tb_kode.TabIndex = 9
+        '
+        'tb_nama
+        '
+        Me.tb_nama.Location = New System.Drawing.Point(158, 169)
+        Me.tb_nama.Name = "tb_nama"
+        Me.tb_nama.Size = New System.Drawing.Size(253, 20)
+        Me.tb_nama.TabIndex = 11
+        '
+        'tb_alamat
+        '
+        Me.tb_alamat.Location = New System.Drawing.Point(158, 200)
+        Me.tb_alamat.Multiline = True
+        Me.tb_alamat.Name = "tb_alamat"
+        Me.tb_alamat.Size = New System.Drawing.Size(445, 73)
+        Me.tb_alamat.TabIndex = 12
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(22, 288)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(109, 13)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "JENIS PENYEWAAN"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(22, 400)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(68, 13)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "POTONGAN"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(22, 372)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(70, 13)
+        Me.Label12.TabIndex = 16
+        Me.Label12.Text = "SUB HARGA"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(22, 346)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(90, 13)
+        Me.Label13.TabIndex = 15
+        Me.Label13.Text = "LAMA / JUMLAH"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(22, 317)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(80, 13)
+        Me.Label14.TabIndex = 14
+        Me.Label14.Text = "HARGA SEWA"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(343, 400)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(46, 13)
+        Me.Label15.TabIndex = 20
+        Me.Label15.Text = "GRAND"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(343, 372)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(40, 13)
+        Me.Label16.TabIndex = 19
+        Me.Label16.Text = "PAJAK"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(343, 345)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(71, 13)
+        Me.Label17.TabIndex = 18
+        Me.Label17.Text = "SUB GRAND"
+        '
+        'cb_jenis
+        '
+        Me.cb_jenis.FormattingEnabled = True
+        Me.cb_jenis.Items.AddRange(New Object() {"Panggung", "Kursi", "Sound System", "Taman"})
+        Me.cb_jenis.Location = New System.Drawing.Point(158, 284)
+        Me.cb_jenis.Name = "cb_jenis"
+        Me.cb_jenis.Size = New System.Drawing.Size(177, 21)
+        Me.cb_jenis.TabIndex = 21
+        '
+        'tb_harga_sewa
+        '
+        Me.tb_harga_sewa.Location = New System.Drawing.Point(158, 314)
+        Me.tb_harga_sewa.Name = "tb_harga_sewa"
+        Me.tb_harga_sewa.Size = New System.Drawing.Size(177, 20)
+        Me.tb_harga_sewa.TabIndex = 22
+        '
+        'tb_jml
+        '
+        Me.tb_jml.Location = New System.Drawing.Point(158, 342)
+        Me.tb_jml.Name = "tb_jml"
+        Me.tb_jml.Size = New System.Drawing.Size(177, 20)
+        Me.tb_jml.TabIndex = 23
+        '
+        'tb_potongan
+        '
+        Me.tb_potongan.Location = New System.Drawing.Point(158, 397)
+        Me.tb_potongan.Name = "tb_potongan"
+        Me.tb_potongan.Size = New System.Drawing.Size(177, 20)
+        Me.tb_potongan.TabIndex = 25
+        '
+        'tb_sub_harga
+        '
+        Me.tb_sub_harga.Location = New System.Drawing.Point(158, 369)
+        Me.tb_sub_harga.Name = "tb_sub_harga"
+        Me.tb_sub_harga.Size = New System.Drawing.Size(177, 20)
+        Me.tb_sub_harga.TabIndex = 24
+        '
+        'tb_grand
+        '
+        Me.tb_grand.Location = New System.Drawing.Point(426, 397)
+        Me.tb_grand.Name = "tb_grand"
+        Me.tb_grand.Size = New System.Drawing.Size(177, 20)
+        Me.tb_grand.TabIndex = 28
+        '
+        'tb_pajak
+        '
+        Me.tb_pajak.Location = New System.Drawing.Point(426, 369)
+        Me.tb_pajak.Name = "tb_pajak"
+        Me.tb_pajak.Size = New System.Drawing.Size(177, 20)
+        Me.tb_pajak.TabIndex = 27
+        '
+        'tb_sub_grand
+        '
+        Me.tb_sub_grand.Location = New System.Drawing.Point(426, 342)
+        Me.tb_sub_grand.Name = "tb_sub_grand"
+        Me.tb_sub_grand.Size = New System.Drawing.Size(177, 20)
+        Me.tb_sub_grand.TabIndex = 26
+        '
+        'btn_keluar
+        '
+        Me.btn_keluar.Location = New System.Drawing.Point(528, 442)
+        Me.btn_keluar.Name = "btn_keluar"
+        Me.btn_keluar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_keluar.TabIndex = 29
+        Me.btn_keluar.Text = "Keluar"
+        Me.btn_keluar.UseVisualStyleBackColor = True
+        '
+        'btn_new
+        '
+        Me.btn_new.Location = New System.Drawing.Point(438, 442)
+        Me.btn_new.Name = "btn_new"
+        Me.btn_new.Size = New System.Drawing.Size(75, 23)
+        Me.btn_new.TabIndex = 30
+        Me.btn_new.Text = "Baru"
+        Me.btn_new.UseVisualStyleBackColor = True
+        '
+        'dtp_tanggal
+        '
+        Me.dtp_tanggal.Location = New System.Drawing.Point(157, 139)
+        Me.dtp_tanggal.Name = "dtp_tanggal"
+        Me.dtp_tanggal.Size = New System.Drawing.Size(200, 20)
+        Me.dtp_tanggal.TabIndex = 31
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(620, 477)
+        Me.Controls.Add(Me.dtp_tanggal)
+        Me.Controls.Add(Me.btn_new)
+        Me.Controls.Add(Me.btn_keluar)
+        Me.Controls.Add(Me.tb_grand)
+        Me.Controls.Add(Me.tb_pajak)
+        Me.Controls.Add(Me.tb_sub_grand)
+        Me.Controls.Add(Me.tb_potongan)
+        Me.Controls.Add(Me.tb_sub_harga)
+        Me.Controls.Add(Me.tb_jml)
+        Me.Controls.Add(Me.tb_harga_sewa)
+        Me.Controls.Add(Me.cb_jenis)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.tb_alamat)
+        Me.Controls.Add(Me.tb_nama)
+        Me.Controls.Add(Me.tb_kode)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "TRANSAKSI PENYEWAAN"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents tb_kode As TextBox
+    Friend WithEvents tb_nama As TextBox
+    Friend WithEvents tb_alamat As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents cb_jenis As ComboBox
+    Friend WithEvents tb_harga_sewa As TextBox
+    Friend WithEvents tb_jml As TextBox
+    Friend WithEvents tb_potongan As TextBox
+    Friend WithEvents tb_sub_harga As TextBox
+    Friend WithEvents tb_grand As TextBox
+    Friend WithEvents tb_pajak As TextBox
+    Friend WithEvents tb_sub_grand As TextBox
+    Friend WithEvents btn_keluar As Button
+    Friend WithEvents btn_new As Button
+    Friend WithEvents dtp_tanggal As DateTimePicker
 End Class
--- transaksiPenyewaan/Form1.vb
+++ transaksiPenyewaan/Form1.vb
@@ -1,3 +1,44 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub cb_jenis_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cb_jenis.SelectedIndexChanged
+        If cb_jenis.SelectedItem = "Panggung" Then
+            tb_harga_sewa.Text = 2000000
+        ElseIf cb_jenis.SelectedItem = "Kursi" Then
+            tb_harga_sewa.Text = 25000
+        ElseIf cb_jenis.SelectedItem = "Sound System" Then
+            tb_harga_sewa.Text = 900000
+        ElseIf cb_jenis.SelectedItem = "Taman" Then
+            tb_harga_sewa.Text = 350000
+        End If
+    End Sub
+
+    Private Sub btn_keluar_Click(sender As Object, e As EventArgs) Handles btn_keluar.Click
+        Close()
+    End Sub
+
+    Private Sub tb_jml_TextChanged(sender As Object, e As EventArgs) Handles tb_jml.TextChanged
+        tb_sub_harga.Text = Val(tb_harga_sewa.Text) * Val(tb_jml.Text)
+        tb_potongan.Text = 0.1 * Val(tb_sub_harga.Text)
+        tb_sub_grand.Text = Val(tb_sub_harga.Text) - Val(tb_potongan.Text)
+        tb_pajak.Text = 0.1 * Val(tb_sub_grand.Text)
+        tb_grand.Text = Val(tb_sub_grand.Text) + Val(tb_pajak.Text)
+    End Sub
+
+    Private Sub btn_new_Click(sender As Object, e As EventArgs) Handles btn_new.Click
+        tb_kode.Clear()
+        dtp_tanggal.ResetText()
+        tb_nama.Clear()
+        tb_alamat.Clear()
+        cb_jenis.Text = ""
+        tb_harga_sewa.Clear()
+        tb_jml.Clear()
+        tb_sub_harga.Clear()
+        tb_potongan.Clear()
+        tb_sub_grand.Clear()
+        tb_pajak.Clear()
+        tb_grand.Clear()
+    End Sub
 End Class
